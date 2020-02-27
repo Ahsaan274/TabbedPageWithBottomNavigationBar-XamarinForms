63244145a0d2170ad06d4bf67a84195d0d5e2ceb
Ahsaan Ansari <ahsaan.se15@gmail.com>
2020-02-27 13:15:38 +0500

    Add project files.

---
+++ Tabbed/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tabbed
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new TabbedPage1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
